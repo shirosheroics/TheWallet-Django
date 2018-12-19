0454c39426d198c923913cfc4d5b32bcd0f42886
Khalid Mohamed <kalmohammed56@gmail.com>
2018-12-19 17:41:09 +0300

    Local server ip added and multiple fixes to support profile location

--- api/models.py
+++ api/models.py
@@ -19,9 +19,9 @@ class Profile(models.Model):
 	balance = models.DecimalField( max_digits=10, decimal_places=3, null=True)
 	savings = models.DecimalField( max_digits=10, decimal_places=3, null=True)
 	automated = models.BooleanField(default=False)
-	latitude = model.CharField(max_length=20,null=True)
-	longitude = model.CharField(max_length=20,null=True)
-	accuracy=model.IntegerField(null=True)
+	latitude = models.CharField(max_length=20,null=True)
+	longitude = models.CharField(max_length=20,null=True)
+	accuracy=models.IntegerField(null=True)
 
 	def __str__(self):
 		return self.user.username
